cf582d8bab65e5becebd4c6367ea6b35a1eff724
mykaharudin <leadload6@gmail.com>
2023-03-14 23:50:00 +0700

    sign up and login display

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     validates_uniqueness_of :username
   scope :all_except, ->(user) { where.not(id: user) }
   after_create_commit { broadcast_append_to "users" }
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  devise_for :users
   get 'rooms/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  devise_scope :user do
+    get 'users',to: 'devise/sessions#new'
+  end
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/signin', to: 'sessions#new'
-  post '/signin', to: 'sessions#create'
-  delete '/signout', to: 'sessions#destroy'
+  # get '/signin', to: 'sessions#new'
+  # post '/signin', to: 'sessions#create'
+  # delete '/signout', to: 'sessions#destroy'
 
-  resources :rooms
-  resources :users
-  root 'rooms#index'
+  # resources :rooms
+  # resources :users
+  # root 'rooms#index'
 end
