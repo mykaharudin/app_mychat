8310bc37012747c2c9c333f69a60d9d7eec90148
mykaharudin <leadload6@gmail.com>
2023-03-14 18:01:26 +0700

    app my chat

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'rooms/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy'
+
+  resources :rooms
+  resources :users
+  root 'rooms#index'
 end
